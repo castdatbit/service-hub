f4cb2a086a4a070f55ca981d65a6c34e2cf8f2f7
castdatbit <castdatbit@gmail.com>
2026-02-25 11:35:04 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,4 +5,5 @@
 //  Înlocuiește valoarea de mai jos cu URL-ul tău Web App:
 //  Ex: 'https://script.google.com/macros/s/AKfy.../exec'
 //
-var API_URL = 'PUNE_URL_APPS_SCRIPT_AICI';
+var API_URL = 'https://script.google.com/macros/s/AKfycbzbRLTZb0dGmGKLVEX-F0kIYjFdxgH_3_x27Ele29wKPa7OVEhk0EngtzQ9OdfxSS7S/exec';
+
